afdcbd9ec473028fbc7d6e01f67c558d57cf078a
Jhon Andrés Parra Rodríguez <ja.parrar12@uniandes.edu.co>
2025-04-20 20:15:46 -0500

    Creacion de funcionalidad para obtener productos

--- src/products/app/repositories/product_repository.py
+++ src/products/app/repositories/product_repository.py
@@ -2,8 +2,16 @@ from app.models.product_model import Product
 from app.core.database import db
 
 class ProductRepository:
-    @staticmethod
-    def create(product: Product):
-        db.session.add(product)
-        db.session.commit()
-        return product
+  
+  @staticmethod
+  def get_all():
+    products = Product.query.all()
+    
+    return products
+  
+  @staticmethod
+  def create(product: Product):
+    db.session.add(product)
+    db.session.commit()
+    
+    return product
--- src/products/app/utils/validate_role.py
+++ src/products/app/utils/validate_role.py
@@ -1,17 +1,16 @@
 from functools import wraps
-from flask import jsonify
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from app.utils.response_util import format_response
 
 def validate_role(role_required):
-    def decorator(fn):
-        @wraps(fn)
-        def wrapper(*args, **kwargs):
-            verify_jwt_in_request()
-            claims = get_jwt()
-            if claims.get("role") == role_required:
-                return fn(*args, **kwargs)
-            else:
-                return format_response("error", 403, error="No posee los permisos necesarios para acceder a este recurso", message="No tiene los permisos necesarios para acceder a este recurso")
-        return wrapper
-    return decorator
+  def decorator(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+      verify_jwt_in_request()
+      claims = get_jwt()
+      if claims.get("role") == role_required:
+        return fn(*args, **kwargs)
+      else:
+        return format_response("error", 403, error="No posee los permisos necesarios para acceder a este recurso", message="No tiene los permisos necesarios para acceder a este recurso")
+    return wrapper
+  return decorator
