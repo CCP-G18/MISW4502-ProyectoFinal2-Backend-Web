4bbf81501116a23a2290fa65cebc5d7d9bb0fb7e
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2025-05-10 18:04:37 -0500

    GPF-87: Creacion de endpoint para previsualizacion de productos en cargue de productos masivo (#88)

--- src/products/app/services/product_service.py
+++ src/products/app/services/product_service.py
@@ -1,6 +1,19 @@
 import uuid
 from app.repositories.product_repository import ProductRepository, Product
 from app.exceptions.http_exceptions import BadRequestError
+from typing import IO
+from app.models.manufacturer_model import Manufacturer
+from app.models.category_model import Category
+import pandas as pd
+
+REQUIRED_COLUMNS = [
+    "Nombre del producto",
+    "Descripción",
+    "Cantidad inicial",
+    "Precio unitario",
+    "Nombre del fabricante",
+    "Nombre de la categoría"
+]
 
 def validate_uuid(id):
   try:
@@ -88,3 +101,66 @@ class ProductService:
     if products is None or len(products) == 0:
         raise BadRequestError("No hay productos en esta categoría")       
     return products
+  
+  @staticmethod
+  def parse_and_validate_file(file):
+    extension_file = file.filename.split('.')[-1].lower()
+    if extension_file not in ['csv', 'xlsx']:
+      raise BadRequestError("Formato no soportado. Solo se permite CSV o Excel")
+    
+    manufacturer_map = {
+      m.name.strip().lower(): m.id for m in Manufacturer.query.all()
+    }
+
+    category_map = {
+      c.name.strip().lower(): c.id for c in Category.query.all()
+    }
+
+    chunksize = 100
+    reader = pd.read_csv(file, chunksize=chunksize) if extension_file == 'csv' else pd.read_excel(file, chunksize=chunksize)
+
+    validos = []
+    errores = []
+
+    for chunk in reader:
+      chunk.fillna('', inplace=True)
+      for _, row in chunk.iterrows():
+        item = row.to_dict()
+        item['errores'] = []
+        for col in REQUIRED_COLUMNS:
+          if str(row.get(col)).strip() == '':
+            item['errores'].append(f"Columna '{col}' vacía")
+
+        name_manufacturer = str(row.get("Nombre del fabricante")).strip().lower()
+        manufacturer_id = manufacturer_map.get(name_manufacturer)
+        if not manufacturer_id:
+          item['errores'].append("Fabricante no encontrado")
+        else:
+          item['manufacturer_id'] = manufacturer_id
+
+        name_category = str(row.get("Nombre de la categoría")).strip().lower()
+        category_id = category_map.get(name_category)
+
+        if not category_id:
+          item['errores'].append("Categoría no encontrada")
+        else:
+          item['category_id'] = category_id
+
+        if not item['errores']:
+          validos.append({
+            "Nombre del producto": row["Nombre del producto"],
+            "Descripción": row["Descripción"],
+            "Cantidad inicial": int(row["Cantidad inicial"]),
+            "Precio unitario": float(row["Precio unitario"]),
+            "manufacturer_id": manufacturer_id,
+            "category_id": category_id,
+            "image_url": None
+          })
+        else:
+          errores.append(item)
+    return {
+      "validos": validos,
+      "errores": errores,
+      "cantidad_validos": len(validos),
+      "cantidad_errores": len(errores)
+    }
